4130a8a176740e2833b6f08a994c5fbe2354604a
martinezdelprenois <martintumwine@gmail.com>
2020-08-27 12:49:59 +0300

    Updates

---
+++ precache-manifest.571e448750b11c6445198ed37c759f24.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433ff79d0dc78475a4bf6564c2ddcaa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46209bec5aa0758750cd",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d79df3a59ec9dbe4ab18",
+    "url": "/static/css/main.2171b6ba.chunk.css"
+  },
+  {
+    "revision": "46209bec5aa0758750cd",
+    "url": "/static/js/2.aea2aeec.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.aea2aeec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79df3a59ec9dbe4ab18",
+    "url": "/static/js/main.a4c9bfd9.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "b6a0eae659e35edfdb2cb014c678c36f",
+    "url": "/static/media/android-studio-logo.b6a0eae6.png"
+  },
+  {
+    "revision": "22925b944293d116eb83ad56fcb334f8",
+    "url": "/static/media/angular.22925b94.png"
+  },
+  {
+    "revision": "900dfc2f4107682ed634cb9fe1caccf8",
+    "url": "/static/media/drupal.900dfc2f.jpg"
+  },
+  {
+    "revision": "d8979dcad356d7e9da5158e96de0f992",
+    "url": "/static/media/image_1.d8979dca.jpg"
+  },
+  {
+    "revision": "a7245edac39aa8b21af82ba89a6c4d2e",
+    "url": "/static/media/ionic.a7245eda.jpeg"
+  },
+  {
+    "revision": "003a9bb51342b68184fb56efbe849076",
+    "url": "/static/media/java.003a9bb5.png"
+  },
+  {
+    "revision": "ce60d93c5bcb148893dcf7cdc60912ee",
+    "url": "/static/media/javascript.ce60d93c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "602a943ca68adbfe5aa2f63cc9ad8a06",
+    "url": "/static/media/mongodb.602a943c.png"
+  },
+  {
+    "revision": "6b47d7fec15d3e1a5de086ac1c808f6d",
+    "url": "/static/media/mysql.6b47d7fe.png"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/static/media/nodejs.6dcf6999.png"
+  },
+  {
+    "revision": "0e6c1e51736a42ce304df182a3f383a7",
+    "url": "/static/media/php.0e6c1e51.png"
+  },
+  {
+    "revision": "a47b04bb1da1f30737fb234240dc33d0",
+    "url": "/static/media/react.a47b04bb.jpg"
+  },
+  {
+    "revision": "31dc05e4ff889c432d1a2f98b962b4fc",
+    "url": "/static/media/wordpress.31dc05e4.png"
+  }
+]);
